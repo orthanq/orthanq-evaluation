ab3a1a4a6eb4671212b5bf2ecaf12998cba918bc
huzuner <hamdiyeuzuner@gmail.com>
2023-11-06 00:06:44 +0000

    sum preprocessing and genotyping and get max of memory c onsumpt.

--- workflow/scripts/runtimes.py
+++ workflow/scripts/runtimes.py
@@ -1,5 +1,3 @@
-#some considerations to be done manually for files:
-#remove the same that was removed rom sample list: SRR071132
 import os
 import glob 
 import pandas as pd
@@ -22,18 +20,20 @@ with open(snakemake.log[0], "w") as f:
     def insert_values(tool_runtimes, df, sample_name, tool_name, filter1_sample_list):
         if (tool_name in tool_runtimes['tool']) or (sample_name in filter1_sample_list):
             idx = tool_runtimes.index[(tool_runtimes['tool'] == tool_name) & (tool_runtimes['sample'] == sample_name)]
-            max_rs = tool_runtimes.iloc[idx]['max_rss']
+            max_rss = tool_runtimes.iloc[idx[0]]['max_rss'] #we know that there will only be one index
 
-            new_max_rs = max_rs
-            new_max_rs += (df.iloc[0]['max_rss'])/1024
+            # new_max_rs = max_rss
+            # new_max_rs += (df.iloc[0]['max_rss'])/1024
+            
+            max_max_rss = max(max_rss, (df.iloc[0]['max_rss'])/1024) # get the max of each processes
+            
+            minutes = tool_runtimes.iloc[idx]['s'] 
 
-            seconds = tool_runtimes.iloc[idx]['s'] 
-
-            new_seconds = seconds
-            new_seconds += (df.iloc[0]['s'])/60
-
-            tool_runtimes.loc[idx, ['max_rss']] = new_max_rs
-            tool_runtimes.loc[idx, ['s']] = new_seconds
+            new_minutes = minutes
+            new_minutes += (df.iloc[0]['s'])/60
+            
+            tool_runtimes.loc[idx, ['max_rss']] = max_max_rss
+            tool_runtimes.loc[idx, ['s']] = new_minutes
         else:
             new_record = pd.DataFrame([{'sample': sample_name, 'tool': tool_name, 'max_rss': (df.iloc[0]["max_rss"])/1024 , 's': (df.iloc[0]["s"])/60}])
             tool_runtimes = pd.concat([tool_runtimes, new_record], ignore_index=True)
@@ -56,19 +56,15 @@ with open(snakemake.log[0], "w") as f:
 
             locus_name = splitted[-1]
             locus_name = locus_name.rsplit('.', 1)[0]
-            print("locus name:")
-            print(locus_name)
             filter1 = tool_runtimes[tool_runtimes["tool"] == tool_name]
             filter1_sample_list = filter1["sample"].tolist()
             
             #check only loci that we evaluate in the paper
-            if (tool_name == "orthanq") or (tool_name == "arcasHLA"):
+            if (tool_name == "orthanq (excl. vg)") or (tool_name == "arcasHLA"):
                 if locus_name == 'A' or locus_name == 'B' or locus_name == 'C' or locus_name == 'DQB1': #varlociraptor from preprocessing, orthanq_call and arcasHLA
                     # update the value of max_rss and s in the final table, for rows having the same tool and sample name
                     tool_runtimes = insert_values(tool_runtimes,df, sample_name, tool_name, filter1_sample_list)
                 elif locus_name == "DQA1" or locus_name == "DRB1":
-                    print(locus_name)
-                    print("continue")
                     continue
                 else:
                     tool_runtimes = insert_values(tool_runtimes, df, sample_name, tool_name, filter1_sample_list)
@@ -97,52 +93,58 @@ with open(snakemake.log[0], "w") as f:
         final = seconds & max_rss
         return final
 
-    ## preprocessing ##
+    ## genotyping/calling with preprocessing combined ##
     for subdir, dirs, files in os.walk(path):
             for dir in dirs:
                 tsv_files = glob.glob(os.path.join(subdir + "/" + dir, "*.tsv"))  
-                if dir == "extract": #arcasHLA
-                    tool_runtimes = collect_measurements("arcasHLA",tsv_files,tool_runtimes, "_")
-                if dir == "samtools_index_after_reheader" or dir == "samtools_reheader" or dir == "samtools_sort" or dir == "samtools_view_primary_chr" or dir == "varlociraptor_preprocess" or dir == "varlociraptor_call": #orthanq
-                    print(dir)
-                    tool_runtimes = collect_measurements("orthanq",tsv_files,tool_runtimes, "_")
-                if dir == "vg_giraffe" or dir == "samtools_view_extract_HLA" or dir ==  "bam_to_fastq": #orthanq + vg
-                    tool_runtimes = collect_measurements("vg (orthanq)",tsv_files,tool_runtimes, "_")
-                if dir == "fastq_split" or dir == "razers3" or dir == "samtools_merge" or dir == "samtools_sort_razers3": #optitype
+                if dir == "hla-la":
+                    tool_runtimes = collect_measurements(dir,tsv_files,tool_runtimes, "_")
+                if dir == "optitype" or dir == "fastq_split" or dir == "razers3" or dir == "samtools_merge" or dir == "samtools_sort_razers3": #optitype
                     tool_runtimes = collect_measurements("optitype",tsv_files,tool_runtimes, "_")
                 if dir == "razers3_bam_to_fastq": #also optitype
                     tool_runtimes = collect_measurements("optitype",tsv_files,tool_runtimes, ".")
-
-    #write the final table to csv
-    tool_runtimes.to_csv(snakemake.output.preprocessing_table, index=False)
-
-    #create plot
-    preprocessing = create_plot(tool_runtimes)
-
-    #write to json
-    preprocessing.save(snakemake.output.preprocessing_plot)
-
-    ## only calling or genotyping ##
-    d = {'sample': [], 'tool': [], "max_rss": [], "s": []}
-    tool_runtimes = pd.DataFrame(data=d)
-
-    ## genotyping/calling ##
-    for subdir, dirs, files in os.walk(path):
-            for dir in dirs:
-                tsv_files = glob.glob(os.path.join(subdir + "/" + dir, "*.tsv"))  
-                if dir == "hla-la" or dir == "optitype":
-                    tool_runtimes = collect_measurements(dir,tsv_files,tool_runtimes, "_")
-                if dir == "orthanq_call":
-                    tool_runtimes = collect_measurements("orthanq",tsv_files,tool_runtimes, "_")
-                if dir == "genotype":
+                if dir == "vg_giraffe" or dir == "samtools_view_extract_HLA" or dir ==  "bam_to_fastq": #orthanq + vg
+                    tool_runtimes = collect_measurements("vg",tsv_files,tool_runtimes, "_")
+                if dir == "orthanq_call" or dir == "samtools_index_after_reheader" or dir == "samtools_reheader" or dir == "samtools_sort" or dir == "samtools_view_primary_chr" or dir == "varlociraptor_preprocess" or dir == "varlociraptor_call":
+                    tool_runtimes = collect_measurements("orthanq (excl. vg)",tsv_files,tool_runtimes, "_")
+                if dir == "genotype" or dir == "extract":
+                    print(dir)
                     tool_runtimes = collect_measurements("arcasHLA",tsv_files,tool_runtimes, "_")
     print(tool_runtimes)
 
+    #separate vg from (orthanq excl. vg)
+    vg = tool_runtimes[tool_runtimes["tool"] == "vg"]
+    orthanq = tool_runtimes[tool_runtimes["tool"] == "orthanq (excl. vg)"]
+
+    #take the sum of vg runtimes from orthanq and vg
+    orthanq_vg_runtime = pd.concat([vg, orthanq]).groupby(["sample"], as_index=False).sum()
+    orthanq_vg_runtime.to_csv("orthanq_plus_vg_runtime.csv", index=False)
+
+    #take the max of orthanq and vg
+    orthanq_vg_max_memory = pd.concat([vg, orthanq]).groupby(["sample"], as_index=False)['max_rss'].max()
+    orthanq_vg_max_memory.to_csv("orthanq_vg_max_memory.csv", index=False)
+
+    #create a new dataframe for vg included orthanq results with new s and max_rss
+    d = {'sample': orthanq_vg_runtime["sample"], 
+        'tool': "orthanq (incl. vg)", 
+        "max_rss": orthanq_vg_max_memory["max_rss"], 
+        "s": orthanq_vg_runtime["s"]}
+    orthanq_plus_vg = pd.DataFrame(data=d)
+
+    #append the vg included df to the tool runtimes df
+    tool_runtimes = pd.concat([tool_runtimes,orthanq_plus_vg])
+
+    #remove rows with "vg"
+    tool_runtimes = tool_runtimes[tool_runtimes["tool"] != "vg"]
+    # tool_runtimes.to_csv("concatenated.csv", index=False)
+
     #write the final table to csv
-    tool_runtimes.to_csv(snakemake.output.genotyping_table, index=False)
+    tool_runtimes.to_csv(snakemake.output.runtimes_table, index=False)
+    # tool_runtimes.to_csv("runtimes.csv", index=False)
 
     #create plot
-    genotyping = create_plot(tool_runtimes)
+    runtimes_plot = create_plot(tool_runtimes)
 
     #write to json
-    genotyping.save(snakemake.output.genotyping_plot)
+    runtimes_plot.save(snakemake.output.runtimes_plot)
+    # runtimes_plot.save("runtimes.json")
